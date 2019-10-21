45692f9135863bdbb6537ca36856c131f7863290
pf2418 <panagiotis.fytas19@imperial.ac.uk>
2019-10-21 22:51:43 +0100

    Some cleanup

--- src/decision_trees.py
+++ src/decision_trees.py
@@ -49,19 +49,14 @@ class Test:
 
 def entropy_calc(label_counters, total_samples):
     entropy = 0
-    # total_samples = sum(label_counters.values())
     base = 2
-
     if total_samples < 1:
         return 0
-
     for label, samples in label_counters.items():
         probability = samples / total_samples
-        # print(samples, total_samples, probability, base)
         if samples == 0:
             continue
         entropy -= probability * math.log(probability, base)
-
     return entropy
 
 
@@ -85,17 +80,13 @@ def read_dataset():
 
 
 def find_split(training_data):
-    print(training_data)
     features = len(training_data[0]) - 1  # calculate number of features
     # calculate the unique value of label and its count
     unique, counts = np.unique(training_data[:, -1], return_counts=True)
     # total_counts is a dictionary that keeps for each label how many times it appears in the dataset
     total_counts = dict(zip(unique, counts))  # form into a dict
     # base entropy is the total entropy of the dataset
-    # print(total_counts, len(training_data))
     base_entropy = entropy_calc(total_counts, len(training_data))
-    # print("1wefe3rf2", total_counts, len(training_data), base_entropy)
-    # base_information_gain = 0.0
     best_feature = -1
     best_feature_value = -1
     max_gain = 0
@@ -103,48 +94,26 @@ def find_split(training_data):
         # the values of each feature e.g. the height of a group of people [170,180,167]
         sorted_data = training_data[training_data[:, i].argsort()]
         feature_list = [datapoint[i] for datapoint in sorted_data]
-        # unique_values = set(feature_list)
-        # sorted_univalue = sorted(list(unique_values))
-        # new_entropy = 0.0
-        # sorted_data = dataMatrix[dataMatrix[:, 1].argsort()]
         prev_val = -10000
         # initialize the left counters
         initial_l_count = []
-        for label in unique:
+        for _label in unique:
             initial_l_count.append(0)
         left_counters = dict(zip(unique, initial_l_count))
         right_counters = total_counts.copy()
         for index, value in enumerate(feature_list):
-            # print("feature: {}, datapoint: {}, value: {}".format(i, index, value))
-            # print("left_counters: {}\n right_counters: {}".format(left_counters, right_counters))
             # if prev_val == value:
             #     continue
             if prev_val != value:
                 current_gain = gain(base_entropy, index, left_counters, len(feature_list) - index, right_counters)
-                # print("Gain: ", current_gain)
-                # print("Total Gain ", base_entropy)
-                # print("feature: {}, datapoint: {}, value: {}".format(i, index, value))
-                # print("left_counters: {}\n right_counters: {}".format(left_counters, right_counters))
-                # sorted_data[:,-1][:index]
-                # sorted_data[:,-1][index:]
                 if current_gain > max_gain:
-                    # print("Gain: ", current_gain)
-                    # print("Total Gain ", base_entropy)
-                    # print("feature: {}, datapoint: {}, value: {}".format(i, index, value))
-                    # print("left_counters: {}\n right_counters: {}".format(left_counters, right_counters))
                     max_gain = current_gain
                     best_feature = i
                     best_feature_value = value
             prev_val = value
             left_counters[sorted_data[index][-1]] += 1
             right_counters[sorted_data[index][-1]] -= 1
-    # print(max_gain)
     return best_feature, best_feature_value
-    # infoGain = gain(base_entropy,)  # 计算当前分类的信息增益
-    # if (infoGain > bestInfoGain):  # 比较那种分类的信息增益最大并返回
-    #     bestInfoGain = infoGain
-    #    bestFeature = i
-    # return bestFeature
 
 
 def split_dataset(training_data, feature, value):
