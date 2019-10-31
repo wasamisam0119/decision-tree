397da4b91b3696958ad35a3a2e98d0b78ec8c58d
pte19 <pierre.el-mqirmi19@imperial.ac.uk>
2019-10-31 16:26:19 +0000

    Depth comparison

--- src/evaluation.py
+++ src/evaluation.py
@@ -53,12 +53,12 @@ def fold_cross_validation(k, file):
     pruned_confusion_matrix = np.array([[0]*4]*4)
 
     # Total classification rate
-    unpruned_total_classification_rate = 0
-    pruned_total_classification_rate = 0
+    unpruned_total_classification_rate = []
+    pruned_total_classification_rate = []
 
     # Depths
-    unpruned_depth = 0
-    pruned_depth = 0
+    total_unpruned_depth = []
+    total_pruned_depth = []
 
     # Loop for the k-fold cross validation
     for i in range(k):
@@ -99,35 +99,32 @@ def fold_cross_validation(k, file):
         pruned_matrix, pruned_classification_rate = evaluate(test_set, best_pruned_tree)
 
         # We get the depth of each trees
-        unpruned_depth += unpruned_tree.get_depth()
-        pruned_depth += best_pruned_tree.get_depth()
+        total_unpruned_depth.append(unpruned_depth)
+        total_pruned_depth.append(best_pruned_tree.get_depth())
 
         # We increment our global confusion_matrices
         unpruned_confusion_matrix = np.add(unpruned_confusion_matrix, unpruned_matrix)
         pruned_confusion_matrix = np.add(pruned_confusion_matrix, pruned_matrix)
 
         # We increment the classification rates
-        unpruned_total_classification_rate += unpruned_classification_rate
-        pruned_total_classification_rate += pruned_classification_rate
+        unpruned_total_classification_rate.append(unpruned_classification_rate)
+        pruned_total_classification_rate.append(pruned_classification_rate)
 
-    # Average classification rate
-    unpruned_average_classification_rate = unpruned_total_classification_rate / k
-    pruned_average_classification_rate = pruned_total_classification_rate / k
-
-    return unpruned_confusion_matrix, unpruned_average_classification_rate, unpruned_depth, \
-           pruned_confusion_matrix, pruned_average_classification_rate, pruned_depth
+    return unpruned_confusion_matrix, unpruned_total_classification_rate, total_unpruned_depth, \
+           pruned_confusion_matrix, pruned_total_classification_rate, total_pruned_depth
 
 
+# Show metrics
 def show_metrics(metrics, title):
 
     # Unpruned Metrics
     unpruned_confusion_matrix = metrics[0]
-    unpruned_average_classification_rate = metrics[1]
+    unpruned_total_classification_rate = metrics[1]
     unpruned_depth = metrics[2]
 
     # Pruned Metrics
     pruned_confusion_matrix = metrics[3]
-    pruned_average_classification_rate = metrics[4]
+    pruned_total_classification_rate = metrics[4]
     pruned_depth = metrics[5]
 
     # Unpruned tree
@@ -152,7 +149,8 @@ def show_metrics(metrics, title):
 
     # We print the matrix easy to read
     print(output_matrix)
-    print("\nAverage Classification Rate: " + str(round(unpruned_average_classification_rate, 4)))
+    print("\nAverage Classification Rate: " +
+          str(round((sum(unpruned_total_classification_rate) / len(unpruned_total_classification_rate)), 4)))
     print("-------------------------------")
 
     # Pruned tree
@@ -176,12 +174,24 @@ def show_metrics(metrics, title):
 
     # We print the matrix easy to read
     print(output_matrix)
-    print("\nAverage Classification Rate: " + str(round(pruned_average_classification_rate, 4)))
+    print("\nAverage Classification Rate: " +
+          str(round((sum(pruned_total_classification_rate) / len(pruned_total_classification_rate)), 4)))
     print("-------------------------------")
+
+    # Depth comparisaon
+    print("\nDepth Comparison:\n")
+    print("Unpruned Tree:")
+    print(unpruned_total_classification_rate)
+    print(unpruned_depth)
+    print("\nPruned Tree:")
+    print(pruned_total_classification_rate)
+    print(pruned_depth)
 
 
 # Get results.
+print("Running Clean Dataset...")
 clean = fold_cross_validation(10, clean_dataset)
+print("Running Noisy Dataset...")
 noisy = fold_cross_validation(10, noisy_dataset)
 show_metrics(clean, "Clean Dataset")
 show_metrics(noisy, "Noisy Dataset")
